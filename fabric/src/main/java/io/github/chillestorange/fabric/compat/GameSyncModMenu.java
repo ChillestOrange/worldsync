b219299964ec725438bc625bd2eb49eb0f0bc14e
ChillestOrange <chillestorange@gmail.com>
2026-06-28 17:16:15 +0530

    refactor: rename to 'compat'

--- fabric/src/main/java/io/github/chillestorange/fabric/compat/GameSyncModMenu.java
+++ fabric/src/main/java/io/github/chillestorange/fabric/compat/GameSyncModMenu.java
@@ -1,10 +1,10 @@
-package io.github.chillestorange.fabric.integration;
+package io.github.chillestorange.fabric.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import io.github.chillestorange.config.GameSyncConfigScreen;
 
-public class ModMenuIntegration implements ModMenuApi {
+public class GameSyncModMenu implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
